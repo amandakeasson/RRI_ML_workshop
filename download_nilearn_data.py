3d1bacffa4767426a1945c7e39d6b8857bacac41
amandakeasson <easson.amanda@gmail.com>
2019-05-03 11:20:27 -0400

    update download script

--- download_nilearn_data.py
+++ download_nilearn_data.py
@@ -7,9 +7,6 @@ warnings.filterwarnings('ignore')
 print('Downloading Haxby dataset')
 datasets.fetch_haxby()
 
-print('Downloading ADHD dataset')
-datasets.fetch_adhd(n_subjects=20)
-
 print('\nDone downloading nilearn data!\n\n')
 print('See you at the workshop. :)\n\n')
 
